0ed3ddf50f5033287bc6d6c2f0ad192db4969d9c
mat3usq <121086125+mat3usq@users.noreply.github.com>
2023-05-11 15:15:34 +0200

    Początkowa Wersja

---
+++ src/main/java/com/example/store_notes/StoreNotesApplication.java
@@ -0,0 +1,13 @@
+package com.example.store_notes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreNotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreNotesApplication.class, args);
+    }
+
+}
