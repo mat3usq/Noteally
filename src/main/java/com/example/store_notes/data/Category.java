f6fa4790c948e0404a58ad4762aeff1e3ef292a0
mat3usq <121086125+mat3usq@users.noreply.github.com>
2023-05-13 13:13:02 +0200

    Add files via upload

---
+++ src/main/java/com/example/store_notes/data/Category.java
@@ -0,0 +1,11 @@
+package com.example.store_notes.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Category {
+    private int idCategory;
+    private String name;
+}
