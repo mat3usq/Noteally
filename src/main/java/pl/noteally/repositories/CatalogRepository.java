06eb86dcb9af5f515b226d705c6d6ca440c2fca3
mat3usq <121086125+mat3usq@users.noreply.github.com>
2023-05-15 22:19:05 +0200

    Delete src directory

--- src/main/java/pl/noteally/repositories/CatalogRepository.java
+++
@@ -1,7 +0,0 @@
-package pl.noteally.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pl.noteally.data.Catalog;
-
-public interface CatalogRepository extends JpaRepository<Catalog, Integer> {
-}
--- src/main/java/pl/noteally/repositories/NoteRepository.java
+++
@@ -1,7 +0,0 @@
-package pl.noteally.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pl.noteally.data.Note;
-
-public interface NoteRepository extends JpaRepository<Note, Integer> {
-}
