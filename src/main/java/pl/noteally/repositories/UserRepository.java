2b11633745654ee68c2022bd7a9eb42a3e5f42d7
mat3usq <121086125+mat3usq@users.noreply.github.com>
2023-05-28 23:05:52 +0200

    Delete src directory

--- src/main/java/pl/noteally/repositories/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package pl.noteally.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pl.noteally.data.User;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
