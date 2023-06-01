d061752000cf3f9973e3c3ee5b4125a64059d476
iza <klis.izabela@gmail.com>
2023-06-01 14:23:11 +0200

    Rejestracja

--- src/main/java/pl/noteally/config/PasswordEncoder.java
+++ src/main/java/pl/noteally/config/PasswordEncoder.java
@@ -7,7 +7,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @Configuration
 public class PasswordEncoder {
     @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+    public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/pl/noteally/config/WebSecurity.java
+++ src/main/java/pl/noteally/config/WebSecurity.java
@@ -3,17 +3,22 @@ package pl.noteally.config;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import pl.noteally.services.UserService;
 
 @Configuration
 @AllArgsConstructor
 @EnableWebSecurity
 public class WebSecurity {
 
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final UserDetailsService userDetailsService;
+    private final BCryptPasswordEncoder passwordEncoder;
+
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -24,5 +29,9 @@ public class WebSecurity {
                         .anyRequest().authenticated()
                 );
         return http.build();
+    }
+
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+                auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
     }
 }
