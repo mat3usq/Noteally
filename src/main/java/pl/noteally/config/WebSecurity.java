17ab2fad49d752e7bffd799af582062ca8968663
xKomaar <jakubo148@gmail.com>
2023-06-01 13:01:57 +0200

    nie wiem co sie dzieje

---
+++ src/main/java/pl/noteally/config/WebSecurity.java
@@ -0,0 +1,28 @@
+package pl.noteally.config;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@AllArgsConstructor
+@EnableWebSecurity
+public class WebSecurity {
+
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .requestMatchers("/css/**", "/images/**","/js/**").permitAll()
+                        .requestMatchers("/registerMe","/**","/login").permitAll()
+                        .anyRequest().authenticated()
+                );
+        return http.build();
+    }
+}
